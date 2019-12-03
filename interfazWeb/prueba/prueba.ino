200f42a1141df5c52dbc77ed9a2e23bb8025d2ad
Shou <nmmarzano@users.noreply.github.com>
2019-12-03 14:44:16 -0300

    Update prueba.ino
    Agregados tres endpoints, prueba de json en /sensores, reformateado el archivo entero

--- interfazWeb/prueba/prueba.ino
+++ interfazWeb/prueba/prueba.ino
@@ -1,110 +1,103 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 
-//Setear credenciales de la red 
+//Setear credenciales de la red
 const char *ssid = "Autito";
 const char *password = "12345678";
-//IPAddress addr(10, 10, 10, 10);
 
-
-//1-instancia de ESP8266WebServer 
-//ESP8266WebServerTemplate(IPAddress addr, int port = 80);
-//ESP8266WebServer server(addr, 80);
+//1-instancia de ESP8266WebServer
 ESP8266WebServer server(80);
- 
-//4-Funciones encargada de enviar la respuesta al cliente
-void handleRoot(){
+
+void sendPlainText(char *plaintext)
+{
     server.sendHeader("Access-Control-Allow-Origin", "*");
-    server.send(200, "text/html", "<h1>Hola Mundo</h1>");
+    server.send(200, "text/plain", plaintext);
 }
- 
+
+void sendJson(char *json)
+{
+    server.sendHeader("Access-Control-Allow-Origin", "*");
+    server.send(200, "application/json", json);
+}
+
+//4-Funciones encargada de enviar la respuesta al cliente
+void handleRoot()
+{
+    sendPlainText("Autito corriendo");
+}
+
 //4-Función que se ejecuta en URL desconocida
-void handleNotFound() {
-  server.sendHeader("Access-Control-Allow-Origin", "*");
-   server.send(404, "text/plain", "Not found");
+void handleNotFound()
+{
+    server.sendHeader("Access-Control-Allow-Origin", "*");
+    server.send(404, "text/plain", "Not found");
 }
- 
-void setup(){
+
+void setup()
+{
     delay(1000);
-    
+
     Serial.begin(9600); //Begin Serial at 115200 Baud 9600
     WiFi.softAP(ssid, password);
-   /* WiFi.begin(ssid, password);
-    IPAddress ip(10,10,10,10);   
-    IPAddress gateway(192,168,1,254);   
-    IPAddress subnet(255,255,255,0);
-    WiFi.config(ip, gateway, subnet);*/
 
-     /*Serial.print("IP address: antes");
-    Serial.println(WiFi.localIP());  //Print the local IP*/
-    
-    /*while (WiFi.status() != WL_CONNECTED) {  //Wait for connection
-      delay(500);
-      Serial.println("Waiting to connect...");
-    }*/
-   
-    
-    if (WiFi.status() != WL_CONNECTED) {  //Wait for connection
-      delay(500);
-      Serial.println("Waiting to connect...");
+    while (WiFi.status() != WL_CONNECTED)
+    { //Wait for connection
+        delay(500);
+        Serial.println("Waiting to connect...");
     }
-    
-  
+
     Serial.print("IP address: despues");
-    Serial.println(WiFi.localIP());  //Print the local IP
-  
+    Serial.println(WiFi.localIP()); //Print the local IP
+
     //2-Definir la asociación entre la URL de la petición y
     //la acción que se ejecutará.
     server.on("/", handleRoot);
-    
-    // Ruteo para '/inline'
-    /*server.on("/inline", []() {
-      server.sendHeader("Access-Control-Allow-Origin", "*");
-        server.send(200, "text/plain", "Esto también funciona");
-    });*/
-    //server.on("/", handle_index); //Handle Index page
 
+    server.on("/frenar", []() {
+        sendPlainText("FRENAR OK");
+    });
+
+    server.on("/masmarcha", []() {
+        sendPlainText("MAS MARCHA OK");
+    });
+
+    server.on("/menosmarcha", []() {
+        sendPlainText("MENOS MARCHA OK");
+    });
 
     server.on("/izquierda", []() {
-      server.sendHeader("Access-Control-Allow-Origin", "*");
-        server.send(200, "text/plain", "IZQUIERDA");
+        sendPlainText("IZQUIERDA OK");
     });
 
     server.on("/derecha", []() {
-      server.sendHeader("Access-Control-Allow-Origin", "*");
-        server.send(200, "text/plain", "DERECHA");
+        sendPlainText("DERECHA OK");
     });
 
     server.on("/adelante", []() {
-      server.sendHeader("Access-Control-Allow-Origin", "*");
-        server.send(200, "text/plain", "ADELANTE");
+        sendPlainText("ADELANTE OK");
     });
 
     server.on("/atras", []() {
-      server.sendHeader("Access-Control-Allow-Origin", "*");
-        server.send(200, "text/plain", "ATRAS");
+        sendPlainText("ATRAS OK");
     });
 
     server.on("/sensores", []() {
-      server.sendHeader("Access-Control-Allow-Origin", "*");
-        server.send(200, "text/plain", "SENSORES");
+        sendJson(
+          "{\"adelante\":0, \"atras\":1, \"izquierda\":2, \"derecha\":3, \"direccion\":4, \"velocidad\":5, \"doblando\":6}"
+        );
     });
-    
+
     // Ruteo para URL desconocida
-    server.onNotFound(handleNotFound);  
-   
-  
-   //Iniciar servidor
+    server.onNotFound(handleNotFound);
+
+    //Iniciar servidor
     server.begin(); //Start the server
     Serial.println("HTTP server started");
 }
- 
-void loop(){
-  //3-función que se encarga de recibir las peticiones de los clientes
-  //y ejecutar las acciones asociadas al ruteo.
-  server.handleClient();
+
+void loop()
+{
+    //3-función que se encarga de recibir las peticiones de los clientes
+    //y ejecutar las acciones asociadas al ruteo.
+    server.handleClient();
 }
-/*void handle_index() {
-  //Print Hello at opening homepage
-  server.send(200, "text/plain", "Hello! This is an index page.");
-}*/
