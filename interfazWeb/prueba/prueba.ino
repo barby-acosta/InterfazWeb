cdd40d2e3456827470b2f6a9191174cdb2476674
Shou <nmmarzano@users.noreply.github.com>
2019-12-03 17:05:17 -0300

    Agregar comunicación serial para comandos
    nueva función sendCommand, variables para mantener la dirección, velocidad y doblado, lógica para enviarla al arduino y sincronizar respuestas

--- interfazWeb/prueba/prueba.ino
+++ interfazWeb/prueba/prueba.ino
@@ -1,4 +1,4 @@
-#include <ESP8266WiFi.h>
+#include <SoftwareSerial.h>
 #include <ESP8266WebServer.h>
 
 //Setear credenciales de la red
@@ -7,6 +7,17 @@ const char *password = "12345678";
 
 //1-instancia de ESP8266WebServer
 ESP8266WebServer server(80);
+
+//Started SoftwareSerial at RX and TX pin of ESP8266/NodeMCU
+SoftwareSerial s(3,1);
+
+// mantener una referencia a los valores actuales para modificarlos
+// ESP tiene la última palabra de esta manera
+int direction = 0;
+int speed = 0;
+int turn = 0;
+
+char buffer[2];
 
 void sendPlainText(char *plaintext)
 {
@@ -18,6 +29,15 @@ void sendJson(char *json)
 {
     server.sendHeader("Access-Control-Allow-Origin", "*");
     server.send(200, "application/json", json);
+}
+
+int sendCommand()
+{
+    s.write('A');
+    s.write(direction);
+    s.write(speed);
+    s.write(turn);
+    return s.read();
 }
 
 //4-Funciones encargada de enviar la respuesta al cliente
@@ -35,6 +55,7 @@ void handleNotFound()
 
 void setup()
 {
+    s.begin(9600);
     delay(1000);
 
     Serial.begin(9600); //Begin Serial at 115200 Baud 9600
@@ -54,31 +75,53 @@ void setup()
     server.on("/", handleRoot);
 
     server.on("/frenar", []() {
-        sendPlainText("FRENAR OK");
+        direction = 0;
+        speed = 0;
+        sendPlainText(itoa(sendCommand(), buffer, 10));
     });
 
     server.on("/masmarcha", []() {
-        sendPlainText("MAS MARCHA OK");
+        speed = speed + 1;
+        if (speed > 3){
+            speed = 3;
+        }
+        sendPlainText(itoa(sendCommand(), buffer, 10));
     });
 
     server.on("/menosmarcha", []() {
-        sendPlainText("MENOS MARCHA OK");
+        speed = speed - 1;
+        if (speed < 0){
+            speed = 0;
+        }
+        sendPlainText(itoa(sendCommand(), buffer, 10));
     });
 
     server.on("/izquierda", []() {
-        sendPlainText("IZQUIERDA OK");
+        if(turn == 2){
+            turn = 0;
+        }else{
+            turn = 1;
+        }
+        sendPlainText(itoa(sendCommand(), buffer, 10));
     });
 
     server.on("/derecha", []() {
-        sendPlainText("DERECHA OK");
+        if(turn == 1){
+            turn = 0;
+        }else{
+            turn = 2;
+        }
+        sendPlainText(itoa(sendCommand(), buffer, 10));
     });
 
     server.on("/adelante", []() {
-        sendPlainText("ADELANTE OK");
+        direction = 1;
+        sendPlainText(itoa(sendCommand(), buffer, 10));
     });
 
     server.on("/atras", []() {
-        sendPlainText("ATRAS OK");
+        direction = 2;
+        sendPlainText(itoa(sendCommand(), buffer, 10));
     });
 
     server.on("/sensores", []() {
